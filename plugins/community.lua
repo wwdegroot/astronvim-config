622c8e58f2573273891f6d63add29c27e17ceecc
Wouter <wouter.w.de.groot@gmail.com>
2023-06-11 20:11:01 +0200

    Update community.lua

--- plugins/community.lua
+++ plugins/community.lua
@@ -1,6 +1,10 @@
 return {
   "AstroNvim/astrocommunity",
-  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.json", enabled = true },
+  { import = "astrocommunity.pack.bash", enabled = true },
+  { import = "astrocommunity.pack.markdown", enabled = true },
+  { import = "astrocommunity.pack.yaml", enabled = true },
+  { import = "astrocommunity.pack.rust", enabled = true },
   { import = "astrocommunity.colorscheme.nightfox", enabled = false },
   { import = "astrocommunity.colorscheme.catppuccin", enabled = true },
   { -- further customize the options set by the community
